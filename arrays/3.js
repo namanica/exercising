19e7b0aace561d2c066e442830b1f967707a5be8
namanica <veronichkapilipna@gmail.com>
2023-12-13 16:06:54 +0200

    test

--- arrays/3.js
+++
@@ -1,6 +0,0 @@
-function unique(arr) {
-    return arr.filter((item, index, array) =>
-        array.indexOf(item) === index);
-
-}
-console.log(unique(['top', 'bottom', 'top', 'left']));
--- basic syntax/8.js
+++
@@ -1,8 +0,0 @@
-function createUser(name, city) {
-  let object = {
-       name: name,
-       city: city,
-  };
-  return object;
-}
-console.log(createUser('Tymur', 'Kyiv'));
